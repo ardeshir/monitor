6e6c74acb613beb094d882935f099c191f2252d0
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-04-22 00:30:56 +0000

    adding static files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
 
@@ -8,6 +9,13 @@ import (
 )
 
 func main() {
+    
+    var port string
+	flag.StringVar(&port, "port", "8000", "port number")
+	flag.Parse()
+	
     http.Handle("/metrics", promhttp.Handler())
-    log.Fatal(http.ListenAndServe(":8000", nil))
+    http.Handle("/", http.FileServer(http.Dir("/var/www/html/public")))
+    // log.Fatal(http.ListenAndServe(":8000", nil))
+    http.ListenAndServe(":"+port, nil)
 }
